b3b74a12fcae618e97480a55cadb5fc9094f79e0
Grace Liu <graceliu.gll@gmail.com>
2024-02-18 04:28:02 -0800

    load data

--- TreeHacksX/back/vector.py
+++ TreeHacksX/back/vector.py
@@ -11,8 +11,8 @@ threshold = 0.5
 def create_connection():
     username = 'SUPERUSER'
     password = 'SYS2' # Replace password with password you set
-    hostname = 'localhost' 
-    port = '1972' 
+    hostname = 'localhost'
+    port = '1972'
     namespace = 'USER'
     CONNECTION_STRING = f"iris://{username}:{password}@{hostname}:{port}/{namespace}"
 
@@ -53,6 +53,7 @@ def insert_photos(client, conn, csv_path):
     DROP TABLE pictures
     """
     result = conn.execute(text(sql))
+
     # Load 
     sql = f"""
         CREATE TABLE pictures (
@@ -91,6 +92,214 @@ def insert_photos(client, conn, csv_path):
             """
     result = conn.execute(text(sql))
     # print(result.fetchall())
+
+
+def insert_stat_card_data(conn):
+
+    # Load 
+    sql = f"""
+    DROP TABLE stat_card_data
+    """
+    result = conn.execute(text(sql))
+
+    # Load 
+    sql = f"""
+    CREATE TABLE stat_card_data (
+    stat_name TEXT NOT NULL,
+    stat_value TEXT NOT NULL,
+    change_indicator TEXT NOT NULL
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO stat_card_data (stat_name, stat_value, change_indicator) VALUES
+    ('Cognitive Performance Score', '0.76', '-8%'),
+    ('Total Memory Cards Attempted', '26', '+7'),
+    ('Mood Today', 'Sad', '-1'),
+    ('Top Memory Category', 'Family & Relations', '0')
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_line_chart_data(conn):
+
+    # Load 
+    sql = f"""
+    DROP TABLE line_chart_data
+    """
+    result = conn.execute(text(sql))
+    # Load 
+    sql = f"""
+    CREATE TABLE line_chart_data (
+    date TEXT NOT NULL,
+    cosine_similarity_score FLOAT
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO line_chart_data (date, cosine_similarity_score) VALUES
+    ('Mon, Feb 12', 0.1),
+    ('Tue, Feb 13', 0.6),
+    ('Wed, Feb 14', 0.3),
+    ('Thu, Feb 15', 0.2),
+    ('Fri, Feb 16', 0.35),
+    ('Sat, Feb 17', 0.15),
+    ('Sun, Feb 18', 0.45)
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_mood_chart_data(conn):
+
+    # Load 
+    sql = f"""
+    DROP TABLE mood_chart_data
+    """
+    result = conn.execute(text(sql))
+
+    # Load 
+    sql = f"""
+    CREATE TABLE mood_chart_data (
+    date TEXT NOT NULL,
+    mood INT
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO mood_chart_data (date, mood) VALUES
+    ('Mon, Feb 12', -1),
+    ('Tue, Feb 13', 0),
+    ('Wed, Feb 14', 0),
+    ('Thu, Feb 15', 1),
+    ('Fri, Feb 16', -1),
+    ('Sat, Feb 17', 1),
+    ('Sun, Feb 18', 0)
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_pie_chart_data(conn):
+    
+    # Load 
+    sql = f"""
+    DROP TABLE pie_chart_data
+    """
+    result = conn.execute(text(sql))
+    # Load 
+    sql = f"""
+    CREATE TABLE pie_chart_data (
+    category TEXT NOT NULL,
+    value INT,
+    fill TEXT NOT NULL
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO pie_chart_data (category, value) VALUES
+    ('Health', 27, 'var(--red-7)'),
+    ('Family', 46, 'var(--green-7)'),
+    ('Retirement', 14, 'var(--purple-7)'),
+    ('Hobbies', 13, 'var(--blue-7)'),
+    ('Technology', 8, 'var(--yellow-7)'),
+    ('Housing', 22, 'var(--pink-7)')
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_bar_chart_data(conn):
+        
+    # Load 
+    sql = f"""
+    DROP TABLE bar_chart_data
+    """
+    result = conn.execute(text(sql))
+
+    # Load 
+    sql = f"""
+    CREATE TABLE bar_chart_data (
+    date TEXT NOT NULL,
+    remember INT,
+    no_remember INT
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO bar_chart_data (date, mood) VALUES
+    ('Mon, Feb 12', 12, -4),
+    ('Tue, Feb 13', 23, -7),
+    ('Wed, Feb 14', 13, -18),
+    ('Thu, Feb 15', 15, -9),
+    ('Fri, Feb 16', 10, -5),
+    ('Sat, Feb 17', 20, -10),
+    ('Sun, Feb 18', 15, -5)
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_radar_chart_data(conn):
+    
+    # Load 
+    sql = f"""
+    DROP TABLE radar_chart_data
+    """
+    result = conn.execute(text(sql))
+
+    # Load 
+    sql = f"""
+    CREATE TABLE radar_chart_data (
+    topic TEXT NOT NULL,
+    value INT,
+    fullMark INT
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO radar_chart_data (topic, value, fullMark) VALUES
+    ('Health', 83, 150),
+    ('Family', 128, 150),
+    ('Retirement', 86, 150),
+    ('Hobbies', 65, 150),
+    ('Technology', 12, 150)
+    """
+    result = conn.execute(text(sql))
+
+
+def insert_tabular_data(conn):
+    # Load 
+    sql = f"""
+    DROP TABLE tabular_data
+    """
+    result = conn.execute(text(sql))
+    # Load 
+    sql = f"""
+    CREATE TABLE tabular_data (
+    name TEXT NOT NULL,
+    username TEXT NOT NULL,
+    interest TEXT NOT NULL
+    )
+    """
+    result = conn.execute(text(sql))
+
+    sql = f"""
+    INSERT INTO tabular_data (name, username, interest) VALUES
+    ('Brad Pita', 'bradpita', 'Family'),
+    ('Scarlet Johamster', 'scarletjohamster', 'Health'),
+    ('Leonardo DiCappuccino', 'leonardodicappuccino', 'Retirement'),
+    ('Taylor Swiss', 'taylorswiss', 'Hobbies'),
+    ('Katy Cherry', 'katycherry', 'Housing'),
+    ('Justin Thyme', 'justinthyme', 'Technology'),
+    ('Oprah Windfree', 'oprahwindfre', 'Health'),
+    ('Johnny Depth', 'johnnydepth', 'Family'),
+    ('Harry Stylesheet', 'harrystylesheet', 'Retirement')
+    """
+    result = conn.execute(text(sql))
+
 
 def get_photo(conn, user_id):
     sql = text(f"""
